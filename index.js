a3ac0aa0937bad8e0261b4a950d5389dcccf98a7
Greg Driza <gdriza@uarts.edu>
2017-09-10 02:10:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendKitten(name)
+{
+  //expects : "Milo", "Otis", "Garfield", "Ralph"
+}
+
+function destructivelyPrependKitten(name)
+{
+  //expects: ["Bob", "Milo", "Otis", "Garfield"]
+}
+
+function destructivelyRemoveLastKitten()
+{
+  //expects ["Milo", "Otis"]
+}
+
+
+function destructivelyRemoveFirstKitten()
+{
+
+  
+}
+
+function appendKitten(name)
+{
+  
+}
+
+function prependKitten(name)
+{
+  
+}
+
+function removeLastKitten()
+{
+  
+}
+
+function removeFirstKitten()
+{
+  
+}
